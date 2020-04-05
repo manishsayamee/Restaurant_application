b4fc49a2180cb804281a05317ef8b58c3d42b3bd
manishsayamee <manishsayamee@gmail.com>
2020-04-05 12:35:45 +0545

    control form

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,13 +1,49 @@
-import React from 'react';
-import { Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import React, {Component}from 'react';
+import { Breadcrumb, BreadcrumbItem, Form, FormGroup, Label, Input,Col, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+class Contact extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            firstname:"",
+            lastname:'',
+            telnum:"",
+            email:'',
+            agree:false,
+            contactType:'Tel.',
+            message:''
 
-function Contact(props) {
+        };
+
+        this.handleInputChange=this.handleInputChange.bind(this);
+        this.handleSubmit=this.handleSubmit.bind(this);
+
+
+    }
+    handleInputChange(event){
+        const target=event.target;
+        const value=target.type ==='checkbox'?target.checked:target.value;
+        const name1=target.name;
+
+        this.setState({
+            [name1]:value
+        });
+
+    }
+
+    handleSubmit(event){
+
+        console.log('current State is:'+ JSON.stringify(this.state));
+        alert('current State is:'+ JSON.stringify(this.state));
+        event.preventDefault();
+
+    }
+
+    render(){
     return(
         <div className="container">
-
-<div className="row">
+            <div className="row">
                 <Breadcrumb>
                     <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
                     <BreadcrumbItem active>Contact Us</BreadcrumbItem>
@@ -17,6 +53,7 @@ function Contact(props) {
                     <hr />
                 </div>                
             </div>
+
             <div className="row row-content">
                 <div className="col-12">
                 <h3>Location Information</h3>
@@ -38,13 +75,102 @@ function Contact(props) {
                 <div className="col-12 col-sm-11 offset-sm-1">
                     <div className="btn-group" role="group">
                         <a role="button" className="btn btn-primary" href="tel:+85212345678"><i className="fa fa-phone"></i> Call</a>
-                        <a role="button" className="btn btn-info" href="867489"><i className="fa fa-skype"></i> Skype</a>
+                        <a role="button" className="btn btn-info"><i className="fa fa-skype"></i> Skype</a>
                         <a role="button" className="btn btn-success" href="mailto:confusion@food.net"><i className="fa fa-envelope-o"></i> Email</a>
                     </div>
                 </div>
             </div>
+            <div className="row row-content">
+                <div className="col-12">
+                    <h3>Send us Your Feedback</h3>
+                </div>
+                <div className='col-12 col-md-9'>
+                    <Form onSubmit={this.handleSubmit}>
+                    <FormGroup row>
+                                <Label htmlFor="firstname" md={2}>First Name</Label>
+                                <Col md={10}>
+                                    <Input type="text" id="firstname" name="firstname"
+                                        placeholder="First Name"
+                                        value={this.state.firstname}
+                                        onChange={this.handleInputChange} />
+                                </Col>
+                    </FormGroup>        
+                        <FormGroup row>
+                            <Label htmlFor="lastname" md={2}>Last Name</Label>
+                            <Col md={10}>
+                                <Input type="text" id="lastname" name="lastname"
+                                placeholder="Last Name"
+                                value={this.state.lastname}
+                                onChange={this.handleInputChange}/>
+                        
+                            </Col>
+                        </FormGroup>
+
+                        <FormGroup row>
+                            <Label htmlFor="telnum" md={2}>Contact</Label>
+                            <Col md={10}>
+                                
+                                <Input type="text" id="telnum" name="telnum"
+                                placeholder="Contact Number"
+                                value={this.state.telnum}
+                                onChange={this.handleInputChange}/>
+                             
+                            </Col>
+                        </FormGroup>
+
+                        <FormGroup row>
+                            <Label htmlFor="email" md={2}>Email</Label>
+                            <Col md={10}>
+                              
+                                <Input type="text" id="email" name="email"
+                                placeholder="Email "
+                                value={this.state.email}
+                                onChange={this.handleInputChange}/>
+                              
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Col md={{size:6, offset:2}}>
+                                <FormGroup check>
+                                    <Label check>
+                                        <Input type="checkbox" name="agree"
+                                        checked={this.state.agree}
+                                        onChange={this.handleInputChange}
+                                        />{' '}<strong>May we contact you?</strong>
+                                    </Label>
+                                </FormGroup>
+                            </Col>
+                        
+                        <Col md={{size:3, offset:1}}>
+                            <Input type="select" name="contactType"
+                            value={this.state.contactType}
+                            onChnage={this.handleInputChange}>
+                                <option>Tel.</option>
+                                <option>Email</option>
+                            </Input>
+                        </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            
+                            <Label htmlFor="message" md= {2}>Your Feedback</Label>
+                            <Col md={10}>
+                                <Input type="textarea" id="message" name="message" rows="12"
+                                value={this.state.message}
+                                onChange={this.handleInputChange}/>
+                            </Col>
+                        </FormGroup>
+                        <FormGroup row>
+                            <Col md={{size:10,offset:2}}>
+                            <Button type="submit" color="primary">  Submit Feedback</Button>
+                            </Col>
+                        </FormGroup>
+                    </Form>
+
+                </div>
+            </div>
         </div>
     );
+}
 }
 
 export default Contact;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,72 +1,63 @@
 import React, { Component } from 'react';
-import {Switch, Route, Redirect } from 'react-router-dom';
-import DishDetail from './DishDetailComponent';
-import Header from "./HeaderComponent";
-import Contact from './ContactComponent';
-import Footer from './FooterComponent';
-import Menu from './MenuComponents';
-import {DISHES} from '../shared/dishes';
 import Home from './HomeComponent';
-import { PROMOTIONS } from '../shared/promotion';
+import Menu from './MenuComponents';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+import DishDetail from './DishDetailComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comment';
+import { PROMOTIONS } from '../shared/promotion';
 import { LEADERS } from '../shared/leader';
-
-
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
 
     constructor(props) {
         super(props);
-
         this.state = {
-            dishes: DISHES,
-            promotions:PROMOTIONS,
-            leaders:LEADERS,
-            comments:COMMENTS
-
-        }
+            dishes: DISHES,  // lifting the state up
+            comments: COMMENTS, 
+            leaders: LEADERS, 
+            promotions: PROMOTIONS
+        };
     }
 
-   
-
     render() {
-
-        const DishWithId=({match})=>{
+        const HomePage = () => {
             return(
-                <DishDetail dish={this.state.dishes.filter((dish)=>dish.id===parseInt(match.params.dishId,10))[0]}
-                comments={this.state.dishes.filter((dish)=>dish.id===parseInt(match.params.dishId,10))[0]}/>
-            )
-        }
-       
+                <Home dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+                    promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+                    leader={this.state.leaders.filter((leader) => leader.featured)[0]}    
+                />
+            );
+          }
 
-        const HomePage=()=>{
-            return(<Home dish={this.state.dishes.filter((dish)=>dish.featured)[0]}
-                            promotion={this.state.promotions.filter((promo)=>promo.featured)[0]}
-                            leader={this.state.leaders.filter((leader)=>leader.featured)[0]}
-                            // comment={this.state.comments.filter((comment)=>comment.featured)[0]}
-                            />)
-        }
+        const DishWithId = ({match}) => {
+            return (
+                // 10 -> base 10
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+                    comments={this.state.comments.filter((comment) => comment.id === parseInt(match.params.dishId,10))}
+                />
+            );
+        }  
 
         return (
-            
-         <div>
-            <Header/>
-             <Switch>
-                <Route path='/home' component={HomePage}/>
-                <Route excat path='/menu' component={()=><Menu dishes={this.state.dishes}/>}/>
-                <Route path='/menu/:dishId' component={DishWithId}/>
-                <Route exact path='/contactus' component={Contact} />}/>
-                <Redirect to ='/home'/>
-            </Switch>
-          <Footer />
-          
-          </div>
-       
-
-     
-          
+            <div>
+                <Header />
+                    <Switch> 
+                        <Route path='/home' component={HomePage} />
+                        <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                        <Route path="/menu/:dishId" component={DishWithId} />
+                        <Route exact path="/contactus" component={Contact} />
+                        <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders} />} />
+                        <Redirect to="/home" />  
+                    </Switch>
+                <Footer />
+            </div>
         );
     }
 }
 
-export default Main; 
+export default Main;
